729d1e02b5f6e0af42811c6a8178870994cfc24a
moncon <monconish@protonmail.com>
2022-11-30 14:19:03 +0100

    Flower model

--- app/Http/Controllers/FlowerController.php
+++ app/Http/Controllers/FlowerController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Flower;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
 
 class FlowerController extends Controller
 {
@@ -18,8 +16,6 @@ class FlowerController extends Controller
     public function show($id)
     {
         $flower = Flower::where('id', $id)->first();
-
-        // $flower = DB::table('flowers')->where('id', $id)->first();
         return response(view('flower-detail', ['flower' => $flower]));
     }
 
@@ -39,6 +35,7 @@ class FlowerController extends Controller
         $newFlower = new Flower;
         $newFlower->name = $request->name;
         $newFlower->price = $request->price;
+        $newFlower->save();
 
         return ($newFlower) ? redirect('/flowers')->with('message', '<p style="display:flex; justify-content: center;"><span style="color:green; text-transform: uppercase; font-size: 1.25rem;">' . $request->name . ' : Insert Successful</span></p>') : back()->with('message', 'Insert Failed');
     }
@@ -46,7 +43,7 @@ class FlowerController extends Controller
     public function editFlowerDetails($id)
 
     {
-        $flower = DB::table('flowers')->where('id', $id)->first();
+        $flower = Flower::find($id)->first;
         return view('update', ['flower' => $flower]);
     }
 
@@ -70,14 +67,12 @@ class FlowerController extends Controller
         $deletedFlower = Flower::find($id);
         $deletedFlower->delete();
 
-        // $deletedFlower = DB::table('flowers')
-        //     ->where('id', $id)->delete();
         return ($deletedFlower) ? redirect('/flowers')->with('message', '<p style="display:flex; justify-content: center;"><span style="color:green; text-transform: uppercase; font-size: 1.25rem;">   Delete Successful</span></p>') : redirect('/update')->with('message', 'Delete Failed');
     }
 
     public function flowerAPI()
     {
-        $flowers = DB::table('flowers')->get();
+        $flowers = Flower::all();
         return response()->json([
             'flowers' => [
                 [
